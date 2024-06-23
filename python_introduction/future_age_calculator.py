bf1f4ed2d43cff7544a11eb2d3a955ab919f416a
Franklin Akinboboye <boyefranklin@gmail.com>
2024-06-23 19:25:56 +0100

    Futureage calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+#Declaration of variables
+principal = 1000  # Principal amount
+rate = 0.05       # Annual interest rate as a decimal
+time = 3          # Time in years
+
+#Simple interest calculation
+interest = principal * rate * time
+
+#Result
+print (f"The simple interest is: {interest}")

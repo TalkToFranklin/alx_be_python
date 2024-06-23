b9b9a10bc344016610b5763e56d78a0a8d24705e
Franklin Akinboboye <boyefranklin@gmail.com>
2024-06-23 19:15:40 +0100

    Covert Hours to Seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,10 @@
+# Create a file named hours_to_seconds.py
+
+#Declaration of variables
+hours = 2  # Number of hours to convert
+
+# Calculate the number of seconds in the given hours
+seconds = hours * 3600
+
+# Print the result
+print (f"{hours} hour(s) is {seconds} seconds.")

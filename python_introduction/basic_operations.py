cda0107077496889e1bbfa3fb075c998bf02e4d3
Franklin Akinboboye <boyefranklin@gmail.com>
2024-06-23 15:02:10 +0100

    Submission of basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#this is a program that performs addition, subtraction, and multiplication on numbers 1 & 2.
+
+#First we define the variables
+number1 = 10
+number2 = 5
+
+# Calculate the sum, difference, and product
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results
+print(f'Addition of {number1} and {number2} is {addition}')
+print(f'Subtraction of {number1} and {number2} is {subtraction}')
+print(f'Multiplication of {number1} and {number2} is {multiplication}')

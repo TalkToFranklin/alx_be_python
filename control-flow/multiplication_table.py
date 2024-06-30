5212200500c65ebc5dfd33ff4443e3b7cafa4770
Franklin Akinboboye <boyefranklin@gmail.com>
2024-06-30 21:59:39 +0100

    Mulitplication Table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,4 +3,9 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 # Generate and print the multiplication table
 for i in range(1, 10):
+    product = number * i
     print(f"{number} * {i} = {number * i}")
+
+# To check the range of the for loop
+for i in range(1, 10):
+    print(f"Iteration {i}")

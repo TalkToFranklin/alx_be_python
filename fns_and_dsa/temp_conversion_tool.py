489d042de837e307657fc87f341149fe5e3ccb15
Franklin Akinboboye <boyefranklin@gmail.com>
2024-07-07 18:26:34 +0100

    A Temperature Conversion Function/Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -36,7 +36,7 @@ def main():
         # Ask the user to enter a temperature
         temperature = float(input("Enter the temperature to convert: "))
         # Ask the user to specify the unit of the temperature
-        unit = input("Is this in Celsius or Fahrenheit? (C/F): ").upper()
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
         
         # Check if the unit is valid and perform the conversion
         if unit == 'C':
